eab6ccba5335333f29d888a99b952d747e5e23df
Matty Jorgensen <me@prplecake.com>
2023-08-10 01:22:00 -0500

    Update nullability

--- src/LinkAce/Entites/Link.cs
+++ src/LinkAce/Entites/Link.cs
@@ -6,11 +6,11 @@ public class Link
     public DateTime CreatedAt { get; set; }
     public DateTime? DeletedAt { get; set; }
     public string? Description { get; set; }
-    public string Icon { get; set; }
+    public string? Icon { get; set; }
     public int Id { get; set; }
     public bool IsPrivate { get; set; }
     public int Status { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public DateTime UpdatedAt { get; set; }
     public string Url { get; set; }
     public int UserId { get; set; }

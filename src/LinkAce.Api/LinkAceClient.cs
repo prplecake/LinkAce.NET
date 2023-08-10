c59692ef604e03d471e46e68998730d401e0b4b0
Matty Jorgensen <me@prplecake.com>
2023-08-10 01:08:33 -0500

    Allow passing in HttpClient to LinkAceClient

--- src/LinkAce.Api/LinkAceClient.cs
+++ src/LinkAce.Api/LinkAceClient.cs
@@ -11,18 +11,23 @@ namespace LinkAce.Api;
 public class LinkAceClient
 {
     private static string? _apiUrl;
-    private static readonly HttpClient Client = new();
+    private static HttpClient _client = new();
+    public LinkAceClient(string linkAceUrl, HttpClient httpClient)
+    {
+        _apiUrl = $"{linkAceUrl}/api/v1";
+        _client = httpClient;
+    }
     public LinkAceClient(string linkAceUrl, string apiToken)
     {
         _apiUrl = $"{linkAceUrl}/api/v1";
         // Setup HttpClient
-        Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
-        Client.DefaultRequestHeaders.UserAgent.Add(Meta.UserAgent);
-        Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
+        _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(MediaTypeNames.Application.Json));
+        _client.DefaultRequestHeaders.UserAgent.Add(Meta.UserAgent);
+        _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiToken);
     }
     public async Task<HttpResponseMessage?> CreateLink(Link link)
     {
-        var response = await Client.PostAsync($"{_apiUrl}/links",
+        var response = await _client.PostAsync($"{_apiUrl}/links",
             new StringContent(JsonConvert.SerializeObject(link), Encoding.UTF8,
                 MediaTypeNames.Application.Json));
         return response;
@@ -33,14 +38,14 @@ public class LinkAceClient
         var query = HttpUtility.ParseQueryString(string.Empty);
         query["query"] = url;
         uriBuilder.Query = query.ToString();
-        var response = await Client.GetAsync(uriBuilder.ToString());
+        var response = await _client.GetAsync(uriBuilder.ToString());
         var obj = JsonConvert.DeserializeObject<SearchLinkResponse>(
             await response.Content.ReadAsStringAsync());
         return obj;
     }
     public async Task<HttpResponseMessage?> UpdateLinkById(int id, Link link)
     {
-        var response = await Client.PatchAsync($"{_apiUrl}/links/{id}",
+        var response = await _client.PatchAsync($"{_apiUrl}/links/{id}",
             new StringContent(JsonConvert.SerializeObject(link), Encoding.UTF8,
                 MediaTypeNames.Application.Json));
         return response;
